e0485cc0b074f94ccf9c5aed1503aabb8f6143d1
ShblAlasd <48160546+ShblAlasd@users.noreply.github.com>
2019-03-24 23:52:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,12 +15,6 @@ client.on('ready', () => {
    console.log(`----------------`);
 });
 
-client2.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`2 Account Online ✩' `);
-   console.log(`----------------`);
-});
-
 
 
 client.on('message', message => {
@@ -74,56 +68,11 @@ if (!rank) return message.reply(' ')
   }
 });
 
-client2.on('message', message => {
-    if(message.content === !+'راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === !+'مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === !+'ريب'){
-        message.channel.send('#rep <@286088294234718209>')
-    }
-});
-
-client2.on('message', message => {
-if (message.content === !+'spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client2.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
 
 
 
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
+
+
 
 
 client.login('NTQ5Njc5OTY4ODM4MzUyOTE5.D3k8kA.daK_KDhI_07N_qqbhdj5XinPcBw');
